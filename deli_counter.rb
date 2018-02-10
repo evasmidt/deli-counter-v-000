da1cd781d3dcf798cf893646b36622d4eac21513
Eva Smidt <shevangeline@gmail.com>
2018-02-10 19:25:41 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,8 @@
 # Write your code here.
 katz_deli = []
 def line (other_deli)
-  if katz_deli == 0
+  if katz_deli.length == 0
+
     puts "The line is currently empty."
 "The line is currently: '#{katz_deli}'"
 end
